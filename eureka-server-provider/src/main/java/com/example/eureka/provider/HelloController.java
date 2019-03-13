14be8eac06a1a1edff9132fbc8c1358a54291f22
bai'jian'zhong <baijianzhong@qianmi.com>
2019-03-13 13:42:56 +0800

    [提交][office]

--- eureka-server-provider/src/main/java/com/example/eureka/provider/HelloController.java
+++ eureka-server-provider/src/main/java/com/example/eureka/provider/HelloController.java
@@ -1,19 +1,14 @@
 package com.example.eureka.provider;
 
-import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Logger;
 
 /**
